9ba85b005b9b91bedb56da422c5ae3dbc2acddb8
erri120 <erri120@protonmail.com>
2020-05-17 14:48:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Plugin Template
+
+Project template for developing [.NET Script Framework](https://www.nexusmods.com/skyrimspecialedition/mods/21294) Plugins.
+
+## Requirements
+
+* [Visual Studio 2019](https://visualstudio.microsoft.com/)
+* [.NET Script Framework](https://www.nexusmods.com/skyrimspecialedition/mods/21294)
---
+++ Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using NetScriptFramework.Tools;
+
+namespace PluginTemplate
+{
+    public static class Utils
+    {
+        private static readonly LogFile LogFile;
+
+        static Utils()
+        {
+            //optional to also use LogFileFlags.IncludeTimestampInFileName
+            LogFile = new LogFile(Plugin.PluginName, LogFileFlags.AppendFile | LogFileFlags.AutoFlush | LogFileFlags.IncludeTimestampInLine);
+        }
+
+        public static void Log(string s)
+        {
+            LogFile.AppendLine(s);
+            //uncomment if you want to display the log message on the HUD, useful when for debugging
+            //NetScriptFramework.SkyrimSE.MenuManager.ShowHUDMessage(s, null, true);
+        }
+
+        public static void Do<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable) action(item);
+        }
+    }
+}
