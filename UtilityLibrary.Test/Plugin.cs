336b62f7d2a4f50f399423f7876fb21fbb25c16d
erri120 <erri120@protonmail.com>
2020-05-17 21:05:19 +0200

    Restructured the project

---
+++ UtilityLibrary.Test/Plugin.cs
@@ -0,0 +1,37 @@
+﻿namespace UtilityLibrary.Test
+{
+    public class Plugin : NetScriptFramework.Plugin
+    {
+        public override string Key => "utility.library.test";
+        public static string PluginName => "Utility Library (TEST)";
+        public override string Name => PluginName;
+        public override int Version => 1;
+
+        public override string Author => "erri120";
+        public override string Website => "https://github.com/erri120/netscriptframework-utility";
+
+        public override int RequiredFrameworkVersion => 9;
+        public override int RequiredLibraryVersion => 13;
+
+        protected override bool Initialize(bool loadedAny)
+        {
+            var utilityLibrary = NetScriptFramework.PluginManager.GetPlugin("utility.library");
+            if (utilityLibrary == null) return false;
+            if (!utilityLibrary.IsInitialized) return false;
+            if (!loadedAny) return false;
+
+            Events.OnCrafting.Register(e =>
+            {
+                Utils.Log($"Crafting an item: {e.TemperForm.CreatedItem.Name}, getting {e.XP} XP");
+            });
+
+            Events.OnTempering.Register(e =>
+            {
+                Utils.Log(
+                    $"Tempering {e.Item.Name}, old quality: {e.OldQuality}, new quality: {e.NewQuality}, getting {e.XP} XP");
+            });
+
+            return true;
+        }
+    }
+}
---
+++ UtilityLibrary.Test/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using NetScriptFramework.Tools;
+
+namespace UtilityLibrary.Test
+{
+    internal static class Utils
+    {
+        private static readonly LogFile LogFile;
+
+        static Utils()
+        {
+            //optional to also use LogFileFlags.IncludeTimestampInFileName
+            LogFile = new LogFile(global::UtilityLibrary.Plugin.PluginName, LogFileFlags.AppendFile | LogFileFlags.AutoFlush | LogFileFlags.IncludeTimestampInLine);
+        }
+
+        internal static void Log(string s)
+        {
+            LogFile.AppendLine(s);
+            //uncomment if you want to display the log message on the HUD, useful when for debugging
+            NetScriptFramework.SkyrimSE.MenuManager.ShowHUDMessage(s, null, true);
+        }
+
+        internal static void Do<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var item in enumerable) action(item);
+        }
+    }
+}
